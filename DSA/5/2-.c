5c716ac45b1faea2524eb353c1024ab453b5e2a7
Parth-Narayan-IIITN <bt24csh052@iiitn.ac.in>
2025-02-19 18:54:44 +0530

    Update 2-.c

--- DSA/5/2-.c
+++ DSA/5/2-.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-// Function to merge two subarrays
+
 void merge(int arr[], int left, int mid, int right) {
     int i = left, j = mid + 1, k = 0;
     int temp[right - left + 1];
 
-    // Merge the two halves into temp[]
     while (i <= mid && j <= right) {
         if (arr[i] <= arr[j])
             temp[k++] = arr[i++];
@@ -13,43 +12,36 @@ void merge(int arr[], int left, int mid, int right) {
             temp[k++] = arr[j++];
     }
 
-    // Copy remaining elements of left subarray
     while (i <= mid)
         temp[k++] = arr[i++];
 
-    // Copy remaining elements of right subarray
     while (j <= right)
         temp[k++] = arr[j++];
 
-    // Copy sorted elements back to original array
     for (i = left, k = 0; i <= right; i++, k++)
         arr[i] = temp[k];
 }
 
-// Iterative Merge Sort function
 void mergeSort(int arr[], int n) {
-    int curr_size;  // Size of subarrays to be merged
-    int left_start; // Starting index of left subarray
+    int curr; 
+    int left;
 
-    // Merge subarrays in bottom-up manner
-    for (curr_size = 1; curr_size < n; curr_size *= 2) {
-        for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
-            int mid = left_start + curr_size - 1;
-            int right_end = (left_start + 2 * curr_size - 1 < n - 1) ? (left_start + 2 * curr_size - 1) : (n - 1);
+    for (curr = 1; curr < n; curr *= 2) {
+        for (left = 0; left < n - 1; left += 2 * curr) {
+            int mid = left + curr - 1;
+            int right = (left + 2 * curr - 1 < n - 1) ? (left + 2 * curr - 1) : (n - 1);
             
-            merge(arr, left_start, mid, right_end);
+            merge(arr, left, mid, right);
         }
     }
 }
 
-// Utility function to print array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-// Driver program
 int main() {
     int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
